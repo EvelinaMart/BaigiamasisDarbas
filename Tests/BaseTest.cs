13abb7f67b8b2f04e87e94735f97755961807d9f
EvelinaMart <e.martinonyte@gmail.com>
2023-11-16 19:23:48 +0200

    Add BaseTest and LoginScenario to Tests

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using Framework;
+using NUnit.Framework;
+
+namespace Tests
+{
+    internal class BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.InitializeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.QuitDriver();
+        }
+    }
+}
---
+++ Tests/LoginScenario.cs
@@ -0,0 +1,49 @@
+﻿using Framework;
+using Framework.Pages;
+using NUnit.Framework;
+
+namespace Tests
+{
+    internal class LoginScenario : BaseTest
+    {
+        [SetUp]
+        public void Open()
+        {
+            Login.Open();
+        }
+        [Test]
+
+        public void LoginWithValidData()
+        {
+            string email = "email37589@gmail.com";
+            string validPassword = "testinePaskyra3";
+
+            Login.CloseCookiesWindow();
+            // Login.ClickButtonInfo();
+            // Login.ClickMenuButtonPrisijungti();
+            Login.EnterEmailAddress(email);
+            Login.EnterPassword(validPassword);
+            Login.ClickButtonPrisijungti();
+
+            Assert.That(Login.ProfileIconExists()); // Profile icon is visible
+        }
+        [Test]
+        public void LoginWithInvalidData()
+        {
+            string email = "email37589@gmail.com";
+            string invalidPassword = "testinePaskyra";
+            string expectedErrorMessageTitle = "REGISTRACIJOS KLAIDA";
+            string expectedErrorMessageBody = "Slaptažodis arba el. paštas yra neteisingi.";
+
+            Login.CloseCookiesWindow();
+            // Login.ClickButtonInfo();
+            // Login.ClickMenuButtonPrisijungti();
+            Login.EnterEmailAddress(email);
+            Login.EnterPassword(invalidPassword);
+            Login.ClickButtonPrisijungti();
+
+            Assert.That(Login.GetErrorMessageBody(), Is.EqualTo(expectedErrorMessageBody));
+            Assert.That(Login.GetErrorMessageTitle(), Is.EqualTo(expectedErrorMessageTitle));
+        }
+    }
+}
