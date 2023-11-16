179446911b0ffabf70f62bc7f25ec3b2e7961ffb
EvelinaMart <e.martinonyte@gmail.com>
2023-11-16 12:59:39 +0200

    Add Driver and Common classes to the Framework

---
+++ Framework/Driver.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Framework
+{
+    public class Driver
+    {
+        static IWebDriver driver;
+
+        internal static void InitializeDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        internal static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        internal static void OpenPage(string url)
+        {
+            driver.Url = url;
+        }
+
+        internal static void QuitDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
