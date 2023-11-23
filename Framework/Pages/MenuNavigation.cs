c955b1af8ccadda6a8e2f7515394c1407feaa0fe
EvelinaMart <e.martinonyte@gmail.com>
2023-11-23 12:01:47 +0200

    Add MenuNavigationScenario

---
+++ Framework/Pages/MenuNavigation.cs
@@ -0,0 +1,48 @@
+﻿namespace Framework.Pages
+{
+    public class MenuNavigation
+    {
+        
+        public static void ClickButtonMenu()
+        {
+            string locator = "(//*[@class='icon-menu'])[2]";
+            Common.WaitForElementToBeVisible(locator);
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickMenuItemEdukacijos()
+        {
+            string locator = "//*[@id='menu-item-85667']";
+            string script = "arguments[0].scrollIntoView(true);";
+            Common.WaitForElementToBeVisible(locator);
+            Common.ExecuteJavascript(script, locator);
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickMenuItemTekstilesDekoravimas()
+        {
+            //string locatorScrollIntoView = "//*[text()='EDUKACIJA – TEKSTILĖS DEKORAVIMAS']";
+            string locator = "//*[text()='EDUKACIJA – TEKSTILĖS DEKORAVIMAS']";
+            string script = "arguments[0].scrollIntoView(true);";
+            Common.ExecuteJavascript(script, locator);
+            Common.ClickElement(locator);
+        }
+
+        public static void ClickButtonArrowTotheRight()
+        {
+            //string locatorScrollIntoView = "//*[@id='nav-below']//*[@class='icon-angle-right']";
+            string locator = "//*[@id='nav-below']//*[@class='icon-angle-right']";
+            string script = "arguments[0].scrollIntoView(true);";
+            Common.ExecuteJavascript(script, locator);
+            Common.ClickElement(locator);
+        }
+
+        public static string GetContactPhoneNumber()
+        {
+            string locator = "//*[@id=\"post-83331\"]/div/div/p[3]";
+            //string locator = "//*[@id='post-83331']/div/div/p[3]/text()[1]";
+            Common.WaitForElementToBeVisible(locator);
+            return Common.GetElementText(locator);
+        }
+    }
+}
---
+++ Tests/MenuNavigationScenario.cs
@@ -0,0 +1,22 @@
+﻿using Framework.Pages;
+using NUnit.Framework;
+
+namespace Tests
+{
+    internal class MenuNavigationScenario : BaseTest
+    {
+        [Test]
+
+        public void ConfirmingCorrectContactDetailsInTheEducationCatalogue()
+        {
+            string expectedContactPhoneNumberText = "Susisiekite +370 630 20570\r\nParašykite info@ornamentai.lt";
+
+            MenuNavigation.ClickButtonMenu();
+            MenuNavigation.ClickMenuItemEdukacijos();
+            MenuNavigation.ClickMenuItemTekstilesDekoravimas();
+            MenuNavigation.ClickButtonArrowTotheRight();
+
+            Assert.That(MenuNavigation.GetContactPhoneNumber(), Is.EqualTo(expectedContactPhoneNumberText));
+        }
+    }
+}
