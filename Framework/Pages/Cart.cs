3555e9d43b0d0b5d1cc1e32ee99398c2e330223e
MartynasVCS <martynasvcs@gmail.com>
2023-11-28 10:11:49 +0200

    Remove unneeded waiting

--- Framework/Pages/Cart.cs
+++ Framework/Pages/Cart.cs
@@ -87,7 +87,7 @@
         {
             string locator = "(//*[@id='gift-card-amount'])[2]";
             string option = "70.00€";
-            System.Threading.Thread.Sleep(5000);
+            //System.Threading.Thread.Sleep(5000);
             Common.WaitForElementToBeVisible(locator);
             Common.SelectFromDropdownMenu(locator, option);
         }
